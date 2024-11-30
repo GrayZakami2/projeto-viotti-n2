fff74aa7f7a0bd64a2d6548aca3110f0793e1535
LucasOmewll <lucasgomes.luc@gmail.com>
2024-11-30 11:07:40 -0300

    fix: db.sql

--- src/main/java/db.sql
+++ src/main/java/db.sql
@@ -1,31 +1,32 @@
+drop database pizzaria;
 create database pizzaria;
 
 use pizzaria;
 
 create table gerentes (
-    cpf varchar(11) primary key not null,
+    cpf varchar(14) primary key not null,
     email varchar(120) unique not null,
     senha varchar(255) not null,
-    telefone varchar(10) unique not null,
-    cnpj varchar(14) unique not null
+    telefone varchar(14) unique not null,
+    cnpj varchar(18) unique not null
 );
 
 create table funcionarios (
-    cpf varchar(11) primary key not null,
+    cpf varchar(14) primary key not null,
     nome varchar(150) not null,
     email varchar(120) unique not null,
-    telefone varchar(10) unique not null,
+    telefone varchar(14) unique not null,
     turno varchar(5),
-    CONSTRAINT chk_turno CHECK turno in ('manhã', 'tarde', 'noite')
+    CONSTRAINT chk_turno CHECK (turno in ('manhã', 'tarde', 'noite'))
 );
 
 create table entregadores (
     documento varchar(14) primary key not null,
     tipo_documento tinyint(1) not null,
     email varchar(120) unique not null,
-    telefone varchar(10) unique not null,
+    telefone varchar(14) unique not null,
     cnh varchar(11) unique not null
-)
+);
 
 create table pizzas (
     id integer primary key auto_increment not null,
@@ -34,7 +35,7 @@ create table pizzas (
     contem_gluten tinyint(1) not null,
     tamanho varchar(8) not null,
     valor numeric(5, 2) not null,
-    CONSTRAINT chk_tamanho CHECK tamanho in ('brotinho','média','grande')
+    CONSTRAINT chk_tamanho CHECK (tamanho in ('brotinho','média','grande'))
 );
 
 create table vendas (
